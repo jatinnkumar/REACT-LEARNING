66c3b4d27f1624ad9ec92a915f2f17ad7fd3d20a
jatinpal9146 <jatinpal9146@gmail.com>
2022-07-28 22:34:46 +0530

    practise of components...

---
+++ src/Heading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Heading() {
+    return (
+        <h1>Created Using Components</h1>
+    );
+}
+
+export default Heading;
---
+++ src/List.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function List() {
+    return (
+        <ol>
+            <li>Netflix</li>
+            <li>Amazon Prime</li>
+            <li>Disney Hotstar</li>
+        </ol>
+    );
+}
+
+export default List;
