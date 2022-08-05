a7a79d188df51407bb1d2c8ac4409e74982f9bc1
jatinpal9146 <jatinpal9146@gmail.com>
2022-08-05 23:00:23 +0530

    Practice of props...

---
+++ src/Card.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './card.css';
+
+function Card(props) {
+    console.log(props);
+    return (
+        <>
+            <div className='card'>
+                <a href={props.link} target="_bla</a>nk">
+                    <img src={props.imgSrc} />
+                </a>
+                <div className="container">
+                    <h4><b>{props.vName}</b></h4>
+                    <a href={props.link} target="_blank">
+                        <button>
+                            Watch Now
+                        </button>
+                    </a>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Card from './Card';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <>
+        <Card imgSrc="https://i.ytimg.com/vi/YMBhQJMrdCs/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAeMKOUsC1Rk4Umz7XW4ZIL_bNEJw" vName="Bhool Bhulaiyaa 2 (Title Track)" link="https://www.youtube.com/watch?v=YMBhQJMrdCs" />
+        <Card imgSrc="https://i.ytimg.com/vi/BddP6PYo2gs/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBkH6mntazw0BSfrHhPwTiqmBIZuQ" vName="Kesariya - Brahmāstra" link="https://www.youtube.com/watch?v=BddP6PYo2gs" />
+        <Card imgSrc="https://i.ytimg.com/vi/ZmcBC9-wAXM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA5o1Ry12mU_0lwdRim8G3XyIAbeA" vName="Qaafirana | Kedarnath" link="https://www.youtube.com/watch?v=ZmcBC9-wAXM" />
+    </>,
+    document.getElementById('root'));
