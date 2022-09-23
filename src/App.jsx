fefa623f7a3fe61023e9b6f7b53ddf26b4de7e65
jatinpal9146 <jatinpal9146@gmail.com>
2022-09-23 23:33:55 +0530

    use of useEffect...

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
-import React, { createContext } from 'react';
-import CompB from './CompB';
-
-
-const FirstName = createContext();
-const LastName = createContext();
+import React, { useEffect, useState } from 'react';
 const App = () => {
+    const [num, setNum] = useState(0);
+    const [num2, setNum2] = useState(0);
+    useEffect(() => {
+        alert("I am clicked !!!");
+    }, [num]);
     return (
         <>
-            <FirstName.Provider value={"Jatin"}>
-                <LastName.Provider value={"Kumar"}>
-                    <CompB />
-                </LastName.Provider>
-            </FirstName.Provider>
+            <button onClick={() => { setNum(num + 1); }}>+</button>
+            <p>{num}</p>
+            <button onClick={() => { setNum2(num2 + 1); }}>+</button>
+            <p>{num2}</p>
         </>
     );
 }
 
 export default App;
-export { FirstName, LastName };
