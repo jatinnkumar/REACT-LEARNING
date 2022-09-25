8f09d8e4d49e574d6904082625e3c1a4a0bb2345
jatinpal9146 <jatinpal9146@gmail.com>
2022-09-25 23:10:17 +0530

    fetched pokemon api...

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 const App = () => {
-    const [num, setNum] = useState(0);
-    const [num2, setNum2] = useState(0);
+    const [num, setNum] = useState();
+    const [characterName, setCharacterName] = useState();
+    const [moves, setMoves] = useState();
+    const colorStyle = {
+        color: 'yellow'
+    };
+    const selectValue = (event) => {
+        setNum(event.target.value);
+    }
+
     useEffect(() => {
-        alert("I am clicked !!!");
-    }, [num]);
+        async function getdata() {
+            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
+            setCharacterName(response.data.name);
+            setMoves(response.data.moves.length);
+            console.log(response);
+        }
+        getdata();
+    });
     return (
         <>
-            <button onClick={() => { setNum(num + 1); }}>+</button>
-            <p>{num}</p>
-            <button onClick={() => { setNum2(num2 + 1); }}>+</button>
-            <p>{num2}</p>
+            <h1>Hello, My Name is <span style={colorStyle}>{characterName}</span> and I Have <span style={{ color: 'red' }}>{moves}</span> moves.</h1>
+            <select value={num} onChange={selectValue}>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="25">25</option>
+            </select>
         </>
     );
 }
