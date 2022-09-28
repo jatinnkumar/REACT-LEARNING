24de443229eec6955000b77f69015cdb123fd48a
jatinpal9146 <jatinpal9146@gmail.com>
2022-09-28 22:53:09 +0530

    use of React Router...

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
 
 const App = () => {
-    const [num, setNum] = useState();
-    const [characterName, setCharacterName] = useState();
-    const [moves, setMoves] = useState();
-    const colorStyle = {
-        color: 'yellow'
-    };
-    const selectValue = (event) => {
-        setNum(event.target.value);
-    }
-
-    useEffect(() => {
-        async function getdata() {
-            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
-            setCharacterName(response.data.name);
-            setMoves(response.data.moves.length);
-            console.log(response);
-        }
-        getdata();
-    });
     return (
         <>
-            <h1>Hello, My Name is <span style={colorStyle}>{characterName}</span> and I Have <span style={{ color: 'red' }}>{moves}</span> moves.</h1>
-            <select value={num} onChange={selectValue}>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="25">25</option>
-            </select>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
         </>
     );
 }
---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import About from './About';
+import Contact from './Contact';
+
+const Home = () => {
+    return (
+        <>
+            <About />
+            <Contact />
+        </>
+    );
+}
+
+export default Home;
